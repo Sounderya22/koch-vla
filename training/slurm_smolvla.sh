c776c60ea136a8be5ca8555bf2db943a4192d2d1
Sounderya Varagur Venugopal <your-github-email@example.com>
2026-04-10 15:24:13 -0400

    Modified training script

--- training/slurm_smolvla.sh
+++ training/slurm_smolvla.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
 #SBATCH --job-name=smolvla-koch
-#SBATCH --gres=gpu:rtx_a6000:1
+#SBATCH --gres=gpu:rtxa6000:1
 #SBATCH --mem=32G
-#SBATCH --cpus-per-task=8
-#SBATCH --time=08:00:00
-#SBATCH --partition=tron
+#SBATCH --cpus-per-task=4
+#SBATCH --time=15:00:00
 #SBATCH --output=/fs/nexus-scratch/vvs22/koch-vla/smolvla/logs/%j.out
 #SBATCH --error=/fs/nexus-scratch/vvs22/koch-vla/smolvla/logs/%j.err
 
-# activate environment
-source ~/.bashrc
+source /nfshomes/vvs22/miniconda3/etc/profile.d/conda.sh
 conda activate lerobot
 
-bash /fs/nexus-scratch/vvs22/koch-vla/training/train_smolvla.sh
+export HF_HOME=/nfshomes/vvs22/.cache/huggingface
+export WANDB_PROJECT=smolvla-koch
+
+cd /fs/nexus-scratch/vvs22/lerobot
+
+lerobot-train \
+  --policy.path=lerobot/smolvla_base \
+  --dataset.repo_id=lerobot/koch_pick_place_5_lego \
+  --batch_size=64 \
+  --steps=20000 \
+  --output_dir=/fs/nexus-scratch/vvs22/koch-vla/smolvla/outputs \
+  --job_name=smolvla_koch_lego \
+  --policy.device=cuda \
+  --wandb.enable=true \
+  --wandb.project=smolvla-koch \
+  --save_freq=5000
